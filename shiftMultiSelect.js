70899700a11d179c9f4e55c683f7820a92cea6d3
ryandougc <47313734+ryandougc@users.noreply.github.com>
2020-04-09 00:17:36 -0700

    Version 1 Complete

---
+++ shiftMultiSelect.js
@@ -0,0 +1,121 @@
+ //Set all checkboxes to blank
+ $(':checkbox').prop('checked', false)
+
+ //Reset the studentArray to empty
+ let studentArray = []
+
+ //Set the lastChecked box to null
+ var lastChecked = null
+
+ function multiSelectCheck() {
+    //Assign variables for checkboxes
+    var $chkboxes = $('.checkRow')
+    var $masterCheck = $("input[name='checkboxMaster']")
+   
+    //When any check box is clicked
+    $chkboxes.click(function(e) {
+        //Reset the student array to be blank
+        studentArray = []
+
+        //Set the last checkbox checked if it didnt exist before
+        if (!lastChecked) {
+            lastChecked = this
+        }
+
+        //If the shift button is held while clicking a box
+        if (e.shiftKey) {
+            var start = $chkboxes.index(this)
+            var end = $chkboxes.index(lastChecked)
+
+            var selectedChecks = $chkboxes.slice(Math.min(start,end), Math.max(start,end)+ 1)
+            selectedChecks.prop('checked', lastChecked.checked)
+        }
+
+        //Assign a variable to find if any checkboxes are unchecked
+        var checkBoxStatus = false
+
+        $chkboxes.each(function() {
+            //Highlight any rows that are checked
+            if($(this).is(':checked')){
+                $(this).parents('tr')
+                .addClass("table-primary")
+            }else {
+                $(this).parents('tr')
+                .removeClass("table-primary")
+            }
+
+            //Add all values to the array that are selected
+            if($(this).is(':checked')) {
+                studentArray.push($(this).val())
+
+                //Make sure the multi-select functions are showing
+                $('.multiSelectTools').show()
+            }
+
+            //If a checkbox is unselected, add a count to the checkCount variable
+            if(!$(this).is(':checked')){
+                checkBoxStatus = true
+            } 
+        })
+
+        //If checkCount > 0, not all check boxes are selected, set the master checkbox to false
+        if(checkBoxStatus){
+            $masterCheck.prop('checked', false)
+        }else {
+            $masterCheck.prop('checked', true)
+        }
+
+        //When the studentArray is emtpy, hide the multi-select functions
+        if (studentArray.length == 0){
+            $('.multiSelectTools').hide()
+        }
+
+        //Set the last checked button to 
+        lastChecked = this
+    });
+
+    //When the master checkbox is selected:
+    $masterCheck.click(function(){
+        //Reset the student array to be blank
+        studentArray = []
+
+        //Set all checkboxes state to the same as the master checkbox
+        $(':checkbox').prop('checked', $masterCheck.is(':checked'))
+
+        //If the master checkbox is checked
+        if($(this).is(':checked')){
+            $chkboxes.each(function() {
+                //Add all values to the array
+                if($(this).is(':checked')) {
+                    studentArray.push($(this).val())
+
+                    //Make sure the multi-select functions are showing
+                    $('.multiSelectTools').show()
+                }
+            })
+
+            //When the studentArray is emtpy, hide the multi-select functions
+            if (studentArray.length == 0){
+                $('.multiSelectTools').hide()
+            }
+
+        } else {
+            //Reset the array
+            studentArray = []
+            
+            //Hide the multi-select functions
+            $('.multiSelectTools').hide()
+        }
+        $chkboxes.each(function() {
+            //Highlight any rows that are checked
+            if($(this).is(':checked')){
+                $(this).parents('tr')
+                .addClass("table-primary")
+            }else {
+                $(this).parents('tr')
+                .removeClass("table-primary")
+            }
+        })
+    });
+
+};
